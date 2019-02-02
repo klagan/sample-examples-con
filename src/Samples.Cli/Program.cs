f52647d3018af191dc8bc8a37de737d8aa4255bf
Kam Lagan <kam.lagan@outlook.com>
2019-02-02 20:45:12 +0000

    initial commit

---
+++ src/Samples.Cli/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Samples.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test(options =>
+            {
+                options.Option1 = "1";
+                options.Option2 = "2";
+                options.Option3 = "3";
+            });
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Test(Action<MyOptions> options)
+        {
+            var t = new MyOptions();
+            options(t);
+        }
+    }
+
+    public class MyOptions
+    {
+        public string Option1 { get; set; }
+
+        public string Option2 { get; set; }
+
+        public string Option3 { get; set; }
+    }
+}
